1d1f929e660ca16346337bbde7dbc71191210210
Alex Mazur <51352526+alexmazr@users.noreply.github.com>
2023-01-29 15:53:04 -0700

    Add files via upload

--- script.py
+++ script.py
@@ -14,36 +14,21 @@ def getStat(source, stat_str):
 
 def extractCardData(card):
     data = card.find("span", class_="infocard-lg-data")
-    smalls = data.find_all("small")
-    number = smalls[0].text[1:]
-    card_link = data.find("a", class_="ent-name")
-    
-    dex_page = requests.get(f'{BASE_URL}{card_link["href"]}')
+    dex_page = requests.get(f'{BASE_URL}{data.find("a", class_="ent-name")["href"]}')
     dex_soup = BeautifulSoup(dex_page.content, "html.parser")
-    names = [name.text for name in dex_soup.find("div", class_="sv-tabs-tab-list") if name != '\n']
-    imgs = [url.find("img")['src'] for url in dex_soup.find_all("picture")[1:]]
-    types = [list(map(lambda x: x.text, tbody.parent.find_all("a"))) for tbody in dex_soup.find_all("th", text="Type") if len(tbody.parent.find_all("a")) > 0]
-    hp = getStat(dex_soup, "HP")
-    attack = getStat(dex_soup, "Attack")
-    defense = getStat(dex_soup, "Defense")
-    sp_atk = getStat(dex_soup, "Sp. Atk")
-    sp_def = getStat(dex_soup, "Sp. Def")
-    speed = getStat(dex_soup, "Speed")
-    total = getStat(dex_soup, "Total")
 
-    print(f'Returning {number}')
     return {
-        "num": number,
-        "img_urls": imgs,
-        "names": names,
-        "hp": hp,
-        "attack": attack,
-        "defense": defense,
-        "sp_atk": sp_atk,
-        "sp_def": sp_def,
-        "speed": speed,
-        "total": total,
-        "types": types
+        "num": data.find("small").text[1:],
+        "img_urls": [url.find("img")['src'] for url in dex_soup.find_all("picture")[1:]],
+        "names": [name.text for name in dex_soup.find("div", class_="sv-tabs-tab-list") if name != '\n'],
+        "hp": getStat(dex_soup, "HP"),
+        "attack": getStat(dex_soup, "Attack"),
+        "defense": getStat(dex_soup, "Defense"),
+        "sp_atk": getStat(dex_soup, "Sp. Atk"),
+        "sp_def": getStat(dex_soup, "Sp. Def"),
+        "speed": getStat(dex_soup, "Speed"),
+        "total": getStat(dex_soup, "Total"),
+        "types": [list(map(lambda x: x.text, tbody.parent.find_all("a"))) for tbody in dex_soup.find_all("th", text="Type") if len(tbody.parent.find_all("a")) > 0]
     }
 
 def unpackAndWriteRow(writer, data):
@@ -68,10 +53,8 @@ soup = BeautifulSoup(page.content, "html.parser")
 infocards = soup.find_all("div", class_="infocard")
 filtered_infocards = (card for card in infocards if isGreaterThan(card, LAST_STORED_MON))
 
-
 with ThreadPoolExecutor(max_workers = 1000) as executor:
     results = executor.map(extractCardData, filtered_infocards)
-
 
 with open("output.csv", 'w', newline='', encoding="utf-8") as file:
     for result in results:
